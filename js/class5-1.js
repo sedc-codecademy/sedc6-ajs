915d2125bd6db163afe61199e53a651a9153ddc2
Igor Joshevski <ijoshevski.svs@one.hostopia.com>
2018-01-17 10:13:58 +0100

    Old way inheritance

--- js/class5-1.js
+++ js/class5-1.js
@@ -24,6 +24,28 @@ function Users() {}
 Users.prototype.doSomething = function() { console.log("Users are doing something") };
 //console.log(User, Users);
 
+/*
+function classA(argument1, argument2)
+{
+    this.argument1 = argument1;
+    this.argument2 = argument2;
+}
+
+function classB(argument1, argument2, argument3)
+{
+    this._super.call(this, argument1, argument2);
+    this.argument3 = argument3;
+}
+
+//Inherit
+classB.prototype = Object.create(classA.prototype);
+
+// set the constructor
+classB.prototype.constructor = classB;
+
+// set the "_super" to the classA constructor function
+Student.prototype._super = classA;
+*/
 
 //In console
 //let user = new User;
