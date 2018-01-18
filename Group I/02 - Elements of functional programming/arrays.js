1d3d47707709154e563b598060bea691b621974c
Wekoslav Stefanovski <swekster@gmail.com>
2018-01-18 20:09:38 +0100

    arrays filter init

--- Group I/02 - Elements of functional programming/arrays.js
+++ Group I/02 - Elements of functional programming/arrays.js
@@ -19,9 +19,58 @@ function cubes(array){
 }
 
 function triples(array){
-    // ...
+    let result = [];
+    for (let index = 0; index < array.length; index++) {
+        let element = array[index];
+        result.push(element + element + element);
+    }
+    return result;
+}
+
+
+function applyOperation(array, operation){
+    let result = [];
+    for (let index = 0; index < array.length; index++) {
+        let element = array[index];
+        result.push(operation(element));
+    }
+    return result;
 }
 
 console.log(collection);
 console.log(squares(collection));
 console.log(cubes(collection));
+console.log(triples(collection));
+console.log("======================")
+console.log(collection);
+console.log(applyOperation(collection, n => n*n));
+console.log(applyOperation(collection, n => n*n*n));
+console.log(applyOperation(collection, n => n+n+n));
+console.log(applyOperation(collection, n => 3*n+1));
+console.log("======================")
+console.log(collection);
+console.log(collection.map(n => n*n));
+console.log(collection.map(n => n*n*n));
+console.log(collection.map(n => n+n+n));
+console.log(collection.map(n => 3*n+1));
+console.log("======================");
+console.log("======================");
+
+function getEvens(array){
+    let result = [];
+    for (let index = 0; index < array.length; index++) {
+        const element = array[index];
+        if (element % 2 === 0){
+            result.push(element);
+        }
+    }
+    return result;
+}
+
+function getOdds(array){
+    // ...
+}
+
+console.log(collection);
+console.log(getEvens(collection));
+console.log(getOdds(collection));
