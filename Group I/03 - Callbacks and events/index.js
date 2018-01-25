aea1d3a9857495f14d5745bdca913fd239dcec00
Wekoslav Stefanovski <swekster@gmail.com>
2018-01-25 19:03:01 +0100

    Set timeout

---
+++ Group I/03 - Callbacks and events/index.js
@@ -0,0 +1,20 @@
+// console.log("One");
+
+// setTimeout(function() {
+//     console.log("Two")
+// }, 1000);
+
+// console.log("Three");
+
+let numnames = ["zero", "one", "two", "three", "four", "five"];
+
+for (let index = 0; index < numnames.length; index++) {
+    const element = numnames[index];
+    console.log(element);
+}
+
+
+// 1. let userData = login(userName, password); // Authentication
+// 2. let isAllowed = canViewImage(userData, imageData); // Authorization;
+// 3. setLike(userData, imageData);
+// 4. Update UI
