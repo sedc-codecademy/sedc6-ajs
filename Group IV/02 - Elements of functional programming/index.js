95c2debfd9052e5b18d64981369c4092fc4ce39f
Wekoslav Stefanovski <swekster@gmail.com>
2018-01-27 14:02:38 +0100

    Added funcitons demo

---
+++ Group IV/02 - Elements of functional programming/index.js
@@ -0,0 +1,99 @@
+let weko = function (n) {
+    return n + n;
+}
+
+let weko2 = weko;
+
+function add(x, y) {
+    console.log("add called");
+    return x + y;
+}
+
+let sub = function (x, y) {
+    return x - y;
+};
+
+let mul = (x, y) => x * y;
+
+// let result = add(4, 5);
+// console.log(result);
+
+// result = sub(10, 3);
+// console.log(result);
+
+// result = mul(3, 6);
+// console.log(result);
+
+let x = 4;
+let y = 7;
+
+function displayResult(x, y, operation) {
+    let result = operation(x, y);
+    console.log(`Result is ${result}`);
+}
+
+// displayResult(4, 5, add);
+// displayResult(10, 3, sub);
+// displayResult(3, 6, mul);
+// displayResult(20, 4, (x, y) => x / y);
+// result = sub(10, 3);
+// console.log(result);
+
+// result = mul(3, 6);
+// console.log(result);
+
+function displayHalfResult(x, y, operation) {
+    if (Math.random() > 0.8) {
+        let result = operation(x, y);
+        console.log(`Result is ${result}`);
+        result = operation(x, y);
+        console.log(`Second Result is ${result}`);
+    } else if (Math.random() > 0.5) {
+        let result = operation(x, y);
+        console.log(`Result is ${result}`);
+    } else {
+        console.log(`No result for you`);
+    }
+}
+
+displayHalfResult(4, 5, add);
+displayHalfResult(10, 3, sub);
+displayHalfResult(3, 6, mul);
+displayHalfResult(20, 4, (x, y) => x / y);
+
+
+let values = [
+    { x: 1, y: 2, op: "+" },
+    { x: 3, y: 4, op: "*" },
+    { x: 7, y: 5, op: "-" }
+];
+
+
+function processValues(values) {
+    let add =(x, y) => x + y; 
+    let operations = {
+        "+": add,
+        "*": (x, y) => x * y,
+        "-": (x, y) => x - y,
+    };
+    let sum = 0;
+
+    for (let index = 0; index < values.length; index++) {
+        const element = values[index];
+        const operation = operations[element.op];
+        const value = operation(element.x, element.y)
+        sum += value;
+    }
+    return sum;
+}
+
+let result = processValues(values);
+
+let names = [
+    { name: "Peter", lastName: "Peterson" },
+    { name: "Rick", lastName: "Roll" },
+    { name: "Blaze", lastName: "Speed" },
+];
+
+let resultFirst = "Peter Peterson & Rick Roll & Blaze Speed";
+let resultSecond = "Peterson, Peter / Roll, Rick / Speed, Blaze"
