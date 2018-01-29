cf6fcf1582c173cf0ea6bdc02b1b0f08494ff943
Wekoslav Stefanovski <swekster@gmail.com>
2018-01-29 19:15:57 +0100

    set timeout intro

--- Group IV/03 - Callbacks and events/index.js
+++ Group IV/03 - Callbacks and events/index.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function () {
         let counter = document.getElementById("counter").valueAsNumber;
         let result = document.getElementById("result");
         result.innerHTML = "";
-        for (var index = 0; index < counter; index++) {
+        for (let index = 0; index < counter; index++) {
             let button = document.createElement("button");
             button.innerHTML = `Button #${index+1}`;
             button.addEventListener("click", () => {
@@ -14,3 +14,17 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
+
+/**
+ * 1. Send user/ pass,  recieve userData // Authentication
+ * 2. Send userData, imageData, recieve permission // Authorization
+ * 3. Send userData, imageData, write to log // Auditing
+ * 4. Send userData, imageData, timestamp, display a blue thumb
+ */
+
+let userData = login(user, pass);
+let isAllowed = getPermission(userData, imageData);
+if (isAllowed) {
+    audit(userData, imageData, timestamp);
+    setLike(userData, imageData, timestamp);
+}
---
+++ Group IV/03 - Callbacks and events/set-timeout.js
@@ -0,0 +1,14 @@
+console.log("one");
+setTimeout(function(){
+    console.log("two");
+}, 2000);
+console.log("three");
+
+let numnames = [
+    "zero", "one", "two", "three", "four", "five",
+    "six", "seven", "eight", "nine", "ten"
+];
+
+
+
+
