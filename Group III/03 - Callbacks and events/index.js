df5fbc8ae5164916bbc19244289900b2bffc588b
Wekoslav Stefanovski <swekster@gmail.com>
2018-01-30 19:24:21 +0100

    added set-timeout

--- Group III/03 - Callbacks and events/index.js
+++ Group III/03 - Callbacks and events/index.js
@@ -1,12 +1,23 @@
-let clickHandler = (event) => {
-    console.log(event);
-};
-
 document.addEventListener("DOMContentLoaded", () => {
     let button = document.getElementById("create");
-    let counter = document.getElementById("counter").valueAsNumber;
+    let result = document.getElementById("result");
 
-    button.addEventListener("click", clickHandler);
-})
+    button.addEventListener("click", () => {
+        let counter = document.getElementById("counter").valueAsNumber;
+        result.innerHTML = "";
+        for (let index = 0; index < counter; index++) {
+            let dugme = document.createElement("button");
+            dugme.innerHTML = `Button #${index+1}`;
+            dugme.addEventListener("click", () => {
+                console.log(`I'm button #${index+1} and I have been clicked`);
+            });
+
+            // dugme.addEventListener("click", ((index)=> () => {
+            //     console.log(`I'm button #${index+1} and I have been clicked`);
+            // })(index));
+            result.appendChild(dugme);
+        }
+    });
+});
 
 
---
+++ Group III/03 - Callbacks and events/set-timeout.js
@@ -0,0 +1,14 @@
+// console.log("one");
+
+// setTimeout(() => {
+//     console.log("two");
+// }, 0);
+
+// console.log("three");
+
+let numnames = [
+    "zero", "one", "two", "three", "four", "five",
+    "six", "seven", "eight", "nine", "ten"
+];
+
+
