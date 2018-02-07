fd53bdcb60d3c1bef925cd8db263b5b64d4e7013
Wekoslav Stefanovski <swekster@gmail.com>
2018-02-07 20:00:18 +0100

    Added writting files example

---
+++ Group II/08 - Node and npm/book-counter.js
@@ -0,0 +1,27 @@
+const fs = require("fs");
+
+const cliParams = process.argv.slice(2);
+
+const fileName = cliParams[0] || "books.json";
+const outFile = cliParams[1] || "count.json";
+
+fs.readFile(fileName, {encoding: "utf-8"}, (err, data) => {
+    if (err) {
+        console.error(`Error caught: ${err}`);
+        return;
+    }
+
+    let books = JSON.parse(data);
+
+    let result = {
+        totalCount: books.length
+    }
+
+    fs.writeFile(outFile, JSON.stringify(result, null, 2), (err)=>{
+        if (err){
+            console.error(`Error writing file ${outFile}: ${err}`);
+        } else {
+            console.log(`Successfully written file ${outFile}`);
+        }
+    })
+})
